f4d789c4efc37102f0d336f985869ffe6fa7f2e5
Naruyasu Ota <Naruyasu_Oota@albus.co.jp>
2019-07-12 18:06:02 +0900

    test

---
+++ src/main/java/sample.java
@@ -0,0 +1,11 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hollow World Git");
+		System.out.println("Hollow World Git2");
+		System.out.println("Hollow World Git3");
+
+	}
+
+}
